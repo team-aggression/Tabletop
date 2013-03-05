87f8c3d5e804e2505707622ab7fef7ba9e1fb7d7
friskydingo <eriksson.david89@gmail.com>
2013-03-05 13:21:46 +0100

    Basic loop working

--- CollaBeat/CollaBeat/MainWindow.xaml.cs
+++ CollaBeat/CollaBeat/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using IrrKlang;
 using System.Windows.Threading;
+using System.Diagnostics;
 
 namespace CollaBeat
 {
@@ -23,11 +24,14 @@ namespace CollaBeat
         const int NROFBEATS = 2;
         const int BPM = 128;
 
+        String BEATSPATH = "Song/";
+
         int BEATCONSTANT;
 
         DispatcherTimer dispatcherTimer;
         ISoundEngine engine;
         List<string> soundList;
+        Stopwatch stopwatch;
 
 		public MainWindow()
 		{
@@ -38,18 +42,57 @@ namespace CollaBeat
             soundList = new List<string>();
             BEATCONSTANT = 1000 * 60 * 8 * NROFBEATS / BPM;
 
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+
             //  DispatcherTimer setup
             dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
             dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
             dispatcherTimer.Start();
 
+            addRemoveBeat("kick2.wav");
+
 			// Insert code required on object creation below this point.
 		}
+
+
+        private void addRemoveBeat(string beatName)
+        {
+            beatName = BEATSPATH + beatName;
+            if (!soundList.Contains(beatName))
+            {
+                soundList.Add(beatName);
+            }
+            else
+            {
+                soundList.Remove(beatName);
+            }
+        }
+
+
+        bool first = true;
 
         private void dispatcherTimer_Tick(object sender, EventArgs e)
         {
 
             // Forcing the CommandManager to raise the RequerySuggested event
+            var time = stopwatch.ElapsedMilliseconds;
+
+            if (((time) >= (BEATCONSTANT)) || first)
+            {
+
+                if (first)
+                    first = false;
+
+                stopwatch.Reset();
+                stopwatch.Start();
+
+                foreach (string sound in soundList)
+                {
+                    engine.Play2D(sound);
+                }
+            } 
+
             CommandManager.InvalidateRequerySuggested();
         }
 
